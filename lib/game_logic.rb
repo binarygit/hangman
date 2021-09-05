d45674e0fba30e6dd3609532650bd8eb92f9973c
binarygit <Inscription.abhi@outlook.com>
2021-09-05 19:53:32 +0545

    refactor: terminate loop from game logic itself
    move the methods in order of appearence

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -4,28 +4,7 @@ module GameLogic
   def game_logic
     input = gets.chomp
     if valid?(input)
-      is_guess?(input) ? point_logic(input) : save_game
-    end
-  end
-
-  def in_mystery_word?(guess)
-    mystery_word.include?(guess)
-  end
-
-  def new_letter?(guess)
-    !guessed_letters.include?(guess)
-  end
-
-  def update_guessed_letters(guess)
-    guessed_letters.push(guess) unless guessed_letters.include?(guess)
-  end
-
-  def replace_dash_with_letter(guess)
-    letter_index_in_mystery_word = mystery_word.each_index.select { |i| mystery_word[i] == guess }
-    guess = ' ' + guess + ' '
-    letter_index_in_mystery_word.each do |i|
-      dash_row.insert(i, guess)
-      dash_row.slice!(i + 1)
+      is_guess?(input) ? point_logic(input) : (save_game; @turns_remaining = 0)
     end
   end
 
@@ -43,6 +22,27 @@ module GameLogic
       update_guessed_letters(guess)
       check_win
     end
+  end
+
+  def new_letter?(guess)
+    !guessed_letters.include?(guess)
+  end
+
+  def in_mystery_word?(guess)
+    mystery_word.include?(guess)
+  end
+
+  def replace_dash_with_letter(guess)
+    letter_index_in_mystery_word = mystery_word.each_index.select { |i| mystery_word[i] == guess }
+    guess = ' ' + guess + ' '
+    letter_index_in_mystery_word.each do |i|
+      dash_row.insert(i, guess)
+      dash_row.slice!(i + 1)
+    end
+  end
+
+  def update_guessed_letters(guess)
+    guessed_letters.push(guess) unless guessed_letters.include?(guess)
   end
 
   def check_win
